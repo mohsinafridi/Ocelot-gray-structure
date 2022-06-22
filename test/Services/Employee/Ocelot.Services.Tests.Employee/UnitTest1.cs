5d077233249de9cea0795c764a6854bcbc3344d4
Mohsin Azam <mohsin.z@dplit.com>
2022-06-22 13:55:11 +0500

    Add project files.

---
+++ test/Services/Employee/Ocelot.Services.Tests.Employee/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Ocelot.Services.Tests.Employee;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/Services/Product/Ocelot.Services.Tests.Product/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Ocelot.Services.Tests.Product;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
